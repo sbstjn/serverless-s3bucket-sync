2272cfd52c74da0089d085c274b2d48d2ba352a4
sbstjn <mail@sbstjn.com>
2017-08-02 12:28:49 +0200

    Update serverless declaration file

--- vendor/serverless.d.ts
+++ vendor/serverless.d.ts
@@ -1,17 +1,43 @@
+declare namespace Serverless {
+  interface Options {
+    stage: string | null
+    region: string | null
+    noDeploy?: boolean
+  }
+  
+  namespace Provider {
+    class Aws {
+      constructor(serverless: Serverless, options: Serverless.Options)
+      
+      getProviderName: () => string
+      getRegion: () => string
+      getServerlessDeploymentBucketName: () => string
+      getStage: () => string
+    }
+  }
+}
+
 declare interface Serverless {
+  init(): Promise<any>
+  run(): Promise<any>
+  
+  setProvider(name: string, provider: Serverless.Provider.Aws): null
+  getProvider(name: string): Serverless.Provider.Aws
+  
+  getVersion(): string
+  
   cli: {
     log(message: string): null
   }
-
+  
   config: {
     servicePath: string
   }
-
+  
   service: {
+    getServiceName(): string
+    getAllFunctions(): string[]
+    
     custom: {}
-  }
-
-  getProvider(name: string): {
-    getRegion: () => string
   }
 }
